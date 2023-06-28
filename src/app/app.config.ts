014cf07e794f6af9394a714cd197bdfa39f55448
kostadin79 <39057409+kostadin79@users.noreply.github.com>
2023-06-28 20:04:35 +0300

    Deprecated modules removed:  ServerModule, ServerTransferStateModule, BrowserModule.withServerTransition (#20)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,16 +8,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { BrowserModule } from '@angular/platform-browser';
-import { SocketService } from "app-core/services/socket.service";
-
-import { providersForStore } from "app-core/store/store-providers";
+import { SocketService } from 'app-core/services/socket.service';
+import { providersForStore } from 'app-core/store/store-providers';
 
 const browserConfig: ApplicationConfig = {
   providers: [
     SocketService,
     importProvidersFrom(
-      BrowserModule.withServerTransition({ appId: 'serverApp' }),
       ServiceWorkerModule.register('ngsw-worker.js', {
         enabled: !isDevMode,
         registrationStrategy: 'registerWhenStable:30000',
